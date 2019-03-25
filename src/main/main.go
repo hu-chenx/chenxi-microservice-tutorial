73aaba7c0ed5c22e714a7c1d320500c74c9b309a
Hu <chehu@Julio.local>
2019-03-25 17:13:42 +0800

    update code

--- src/main/main.go
+++ src/main/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"strconv"
 
 	micro "github.com/micro/go-micro"
 	proto "proto"
@@ -11,8 +12,28 @@ import (
 type Palindrome struct{}
 
 func (g *Palindrome) FindPalindrome(ctx context.Context, req *proto.PalindromeRequest, rsp *proto.PalindromeResponse) error {
-	rsp.Palindrome = "Hello " + req.Input
+	rsp.Palindrome = "Hello " +   strconv.Itoa(longestPalindrome(req.Input))
 	return nil
+}
+
+func longestPalindrome(input string) int {
+    m := make(map[rune]int)
+    for _, v := range input {
+        m[v]++
+    }
+    var len int
+    var odd bool
+    for _, vv := range m {
+        len += vv
+        if vv % 2 == 1 {
+            len--
+            odd = true
+        }
+    }
+    if odd {
+        len++
+    }
+    return len
 }
 
 func main() {
